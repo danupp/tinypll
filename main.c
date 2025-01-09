be90cd22b81417d61dfe470db8af58166275b5f8
danupp <vfz.daniel@gmail.com>
2025-01-09 11:32:52 +0100

    draft1

--- main.c
+++ main.c
@@ -29,8 +29,33 @@ fref = external reference frequency
 //#define FTW 819636907  9.38
 //#define FTW 828375040  // 9.48 MHz at 256 div and 19.2 MHz
 //#define FTW 671088640  // 12.5 kHz -> 12.8 MHz at 1024 div
-#define FTW 1006632960  // 19.2e6/1024 at 10/125 MHz
+//#define FTW 1006632960  // 19.2e6/1024 at 10/125 MHz
 
+#define REF_10MHZ_LOWT 1884
+#define REF_10MHZ_HIGHT 2212
+#define REF_20MHZ_LOWT 3768
+#define REF_20MHZ_HIGHT 4423
+#define REF_26MHZ_LOWT 4899
+#define REF_26MHZ_HIGHT 5751
+
+enum ref_freqs {null, _10MHz, _20MHz, _26MHz};
+
+struct lock_freq {
+  uint16_t val_10MHz;
+  uint32_t FTW_10MHz;
+  uint8_t div; // EVSYS.ASYNCCH3
+};
+
+//   uint16_t val_20MHz;
+//   uint32_t FTW_20MHz;
+//   uint16_t val_26MHz;
+//   uint32_t FTW_26MHz;
+//   uint8_t flags;
+// };
+
+ref_freqs ref_freq = null;
+
+volatile uint8_t full_count = 0;
 uint32_t nco_reg;
 uint8_t phase;
 const uint8_t costable[256] =
@@ -43,6 +68,16 @@ ISR(TCA0_OVF_vect) { // 80 kHz @ 10 MHz
   nco_reg += FTW;
   phase = (nco_reg >> 24);
   //PORTA.OUT &= ~LED;
+}
+
+ISR(TCD0_OVF_vect) { 
+  TCD0.SINGLE.INTFLAGS = TCD0_OVF_bm; // clear flag
+  full_count++;
+}
+
+ISR(RTC_PIT_vect) { 
+  RTC0.PITINTFLAGS = 0x01; // clear flag
+  full_count++;
 }
 
 void main () {
@@ -84,6 +119,103 @@ void main () {
   wdr();
 
   _delay_ms(10);
+
+
+
+  // **** Ok we have ref clock, but at what freq?
+  // Let TCD0 run at 20 MHz int osc and count ref at TCA0
+
+  TCD0.INTCTRL = OVF_bm;
+  while (!(TCD0.STATUS & ENRDY_bm));
+  TCD0.CTRLA = 0x01; // Start counting with internal 20 MHz clock
+
+  sei();
+  
+  while(1) {
+    wdr();
+    if (1 == full_count)
+      TCA0.SINGLE.CTRLA = 0x01; // Enable counting with ext clk
+    else if (2 == full_count)
+      break;
+  }
+
+  cli();
+
+  TCA0.SINGLE.CTRLA = 0x00;  // Stop
+  while (!(TCD0.STATUS & ENRDY_bm));
+  TCD0.CTRLA = 0x00; // Stop
+
+  if ((TCA0.CNT > REF_10MHZ_LOWT) && (TCA0.CNT < REF_10MHZ_HIGHT)) {
+    ref_freq = _10MHz;
+  }
+  else if ((TCA0.CNT > REF_20MHZ_LOWT) && (TCA0.CNT < REF_20MHZ_HIGHT)) {
+    ref_freq = _20MHz;
+  }
+  else if ((TCA0.CNT > REF_26MHZ_LOWT) && (TCA0.CNT < REF_26MHZ_HIGHT)) {
+    ref_freq = _26MHz;
+  }
+
+  // *** Now try to measure the VCXO frequency using PIT and TCA0
+
+  full_count = 0;
+  TCA0.CNT = 0;
+
+  while(RTC.STATUS);
+  RTC.CLKSEL = 0x02; // Use XOSC32K/TOSC1 - i.e. VCXO
+  while(RTC.PITSTATUS);
+  RTC.PITINTCTRL = 0x01;
+  while(RTC.PITSTATUS);
+  RTC.PITCTRLA = (0xb << 3) | 0x01; // Enable, 4096 cycles to interrupt
+
+  sei();
+  
+  while(1) {
+    wdr();
+    if (1 == full_count)
+      TCA0.SINGLE.CTRLA = 0x01; // Enable counting with ext clk
+    else if (2 == full_count)
+      break;
+  }
+
+  cli();
+
+  TCA0.SINGLE.CTRLA = 0x00;  // Stop
+  while(RTC.PITSTATUS);
+  RTC.PITCTRLA = 0x00;
+  while(RTC.PITSTATUS);
+  RTC.PITINTCTRL = 0x00;
+
+  struct lock_freq freqs[2] = { 
+    {4915, 1006632960},  // 12 MHz
+    {5898, 1006632960},  // 14.4 MHz
+    {7864, 1006632960, 0x0d},  // 19.2 MHz, 1024
+    {8192, 1006632960},  // 20 MHz
+    {51200, 1006632960, 0x0a},  // 125 MHz, 8192
+    };
+
+  for (int i = 0; i < 2) {
+    if (_10MHz == ref_freq) {
+      if (TCA0.CNT > freqs[i].val_10MHz*0.99 && TCA0.CNT < freqs[i].val_10MHz*1.01) {
+        FTW = freqs[i].FTW;
+        EVSYS.ASYNCCH3 = freqs[i].div;
+        break;
+      }
+    }
+    else if (_20MHz == ref_freq) {
+      if (TCA0.CNT > freqs[i].val_10MHz*0.495 && TCA0.CNT < freqs[i].val_10MHz*0.505) {
+        FTW = freqs[i].FTW;
+        EVSYS.ASYNCCH3 = freqs[i].div;
+        break;
+      }
+    }
+    else if (_26MHz == ref_freq) {
+      if (TCA0.CNT > freqs[i].val_10MHz*0.381 && TCA0.CNT < freqs[i].val_10MHz*0.388) {
+        FTW = freqs[i].FTW;
+        EVSYS.ASYNCCH3 = freqs[i].div;
+        break;
+      }
+    }
+  }
 
   //PORTA.OUT |= LED;
   //for(int i=0; i<10; i++) {
